a61a7c432277b17789551fd78e81c19a85458f6f
keysbyprosper <prosperademoye@gmail.com>
2023-06-05 15:34:52 -0600

    done the navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Explore from "./pages/Explore";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignOut from "./pages/SignOut";
+import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-    <h1>Hello world</h1>
-    </>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/SignOut" element={<SignOut />} />
+          <Route path="/Forgot-Password" element={<ForgotPassword />} />
+          <Route path="/Offers" element={<Offers />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/SignUp" element={<SignUp />} />
+        </Routes>
+        <Navbar />
+      </Router>
+    </div>
   );
 }
 
